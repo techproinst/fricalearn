6d0d41814278c0d57041f96c3db09b6b44276db1
swiftly01 <kareemkazeem100@gmail.com>
2025-03-21 16:26:28 +0100

    student dashboard added

--- scripts/payment.js
+++ scripts/payment.js
@@ -1,10 +1,10 @@
 document
-.getElementById("receiptUpload")
-.addEventListener("change", function () {
-  let fileName =
-    this.files.length > 0 ? this.files[0].name : "File Name";
-  document.querySelector(".file-name").textContent = fileName;
-});
+  .getElementById("receiptUpload")
+  .addEventListener("change", function () {
+    let fileName =
+      this.files.length > 0 ? this.files[0].name : "File Name";
+    document.querySelector(".file-name").textContent = fileName;
+  });
 
 const buttons = document.querySelectorAll(".account");
 const nairaBtn = document.querySelector(".naira-btn");
@@ -17,23 +17,26 @@ nairaDetails.style.display = "block";
 poundsDetails.style.display = "none";
 
 buttons.forEach((btn, index) =>
-btn.addEventListener("click", () => {
-  buttons.forEach((b) => b.classList.remove("clicked"));
+  btn.addEventListener("click", () => {
+    buttons.forEach((b) => b.classList.remove("clicked"));
 
-  if (index === 1) {
-    nairaBtn.classList.remove("clicked");
-    nairaDetails.style.display = "none";
-    poundsDetails.style.display = "block";
-    poundsBtn.classList.add("clicked");
-  }
-  if (index === 0) {
-    poundsBtn.classList.remove("clicked");
-    nairaDetails.style.display = "block";
-    poundsDetails.style.display = "none";
-    nairaBtn.classList.add("clicked");
-  }
+    if (index === 1) {
+      nairaBtn.classList.remove("clicked");
+      nairaDetails.style.display = "none";
+      poundsDetails.style.display = "block";
+      poundsBtn.classList.add("clicked");
+    }
+    if (index === 0) {
+      poundsBtn.classList.remove("clicked");
+      nairaDetails.style.display = "block";
+      poundsDetails.style.display = "none";
+      nairaBtn.classList.add("clicked");
+    }
 
-  btn.classList.add("clicked");
-})
+    btn.classList.add("clicked");
+  })
 );
 
+document.querySelectorAll(".account").forEach((button) => button.addEventListener('click', () => {
+
+}))
