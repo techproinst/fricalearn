d5a7347d5f3c9d917e4babee4034c872e5f2fda1
Dahud Yusuf <abiodunyusuf4@gmail.com>
2025-06-30 21:06:10 +0100

    I modified the Lorem in the Frontend

--- resources/views/pages/igbo_courses.blade.php
+++ resources/views/pages/igbo_courses.blade.php
@@ -54,11 +54,11 @@
           </div>
         </div>
       </div>
-        
+
       @empty
-       <p>No course available yet!!</p>  
+       <p>No course available yet!!</p>
       @endforelse
-     
+
     </div>
   </div>
 </section>
@@ -70,9 +70,7 @@
     </h1>
 
     <p class="text-muted text-center text-md-start">
-      Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget
-      elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget
-      habitasse in velit fringilla feugiat senectus in.
+      Take your language journey further by exploring our Yorùbá and Hausa courses! Each one is designed with the same kid-friendly, engaging style—so your child can discover even more of Africa’s rich cultures and languages in a fun, easy way.
     </p>
 
     <div class="row row-cols-1 row-cols-md-2 row-cols-lg-2 g-5 my-2">
