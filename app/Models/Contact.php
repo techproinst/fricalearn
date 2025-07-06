5efe82fe31bf7f8dffde1f97a87eac3b0ea513fa
swiftly01 <kareemkazeem100@gmail.com>
2025-07-06 16:29:54 +0100

    fixing scheduler

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -5,6 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
-{
+{     
     protected $guarded = [];
 }
--- routes/console.php
+++ routes/console.php
@@ -4,11 +4,14 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
 
+// Your custom command
 Schedule::command('app:process-expired-subscriptions')->everyMinute()->runInBackground();
 
+// Process queued jobs
+Schedule::command('queue:process')->everyMinute()
+    ->appendOutputTo(storage_path('logs/queue.log'))
+    ->runInBackground();
 
-
+// Optional test
+Schedule::command('inspire')->everyMinute()->appendOutputTo(storage_path('logs/test.log'));
