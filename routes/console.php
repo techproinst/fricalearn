a657abfa02847814eb85e42e8cc7d7e2e43fa5fe
swiftly01 <kareemkazeem100@gmail.com>
2025-07-06 11:36:57 +0100

    schedule command

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,7 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('app:process-expired-subscriptions')->everyMinute();
+
+Schedule::command('queue:work')->everyFiveMinutes();
+
+
