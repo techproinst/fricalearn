9bc839df7b05c1ec5ae6bb0ed32af7af140a6abb
swiftly01 <kareemkazeem100@gmail.com>
2025-07-06 20:15:11 +0100

    fixing scheduler

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
            CourseSeeder::class,
            CourseLevelSeeder::class,
-          //  UserSeeder::class,
+            UserSeeder::class,
            TimezoneGroupSeeder::class,
         ]);
         
