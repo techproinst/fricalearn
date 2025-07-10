99a2d3a26b9adc897109134b758224bb0e6d8ac1
swiftly01 <kareemkazeem100@gmail.com>
2025-07-10 07:55:11 +0100

    new user seeded

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,21 +15,21 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         DB::table('users')->insert([
-            // [
-            //     'name' => 'admin',
-            //     'email' => 'testadmin@gmail.com',
-            //     'password' => Hash::make('1234'),
-            // ],
             [
-                'name' => 'abiodun',
-                'email' => 'abiodunyusuf4@gmail.com',
+                'name' => 'dahud',
+                'email' => 'dahudyusufishola@gmail.com',
                 'password' => Hash::make('1234'),
             ],
-            [
-                'name' => 'kazeem',
-                'email' => 'kareemkazeem100@gmail.com',
-                'password' => Hash::make('1234'),
-            ]
+            // [
+            //     'name' => 'abiodun',
+            //     'email' => 'abiodunyusuf4@gmail.com',
+            //     'password' => Hash::make('1234'),
+            // ],
+            // [
+            //     'name' => 'kazeem',
+            //     'email' => 'kareemkazeem100@gmail.com',
+            //     'password' => Hash::make('1234'),
+            // ]
 
 
 
